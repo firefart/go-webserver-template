2a631adc7a6e6b8f468e4d6518525366eba54cdf
firefart <105281+firefart@users.noreply.github.com>
2024-08-10 00:05:50 +0200

    update

---
+++ internal/database/db_test.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"io/fs"
+	"testing"
+
+	"github.com/pressly/goose/v3"
+	"github.com/stretchr/testify/require"
+
+	_ "modernc.org/sqlite"
+)
+
+func TestMigrations(t *testing.T) {
+	db, err := sql.Open("sqlite", ":memory:?_pragma=journal_mode(WAL)&_pragma=foreign_keys(1)")
+	require.Nil(t, err, "could not open database")
+
+	migrationFS, err := fs.Sub(embedMigrations, "migrations")
+	require.Nil(t, err, "could not sub migration fs")
+
+	prov, err := goose.NewProvider("sqlite3", db, migrationFS)
+	require.Nil(t, err, "could not create goose provider")
+
+	ctx := context.Background()
+
+	result, err := prov.Up(ctx)
+	require.Nil(t, err, "could not apply migrations")
+
+	for _, r := range result {
+		if r.Error != nil {
+			require.Nilf(t, r.Error, "could not apply migration %s", r.Source.Path)
+		}
+	}
+
+	require.Positive(t, len(result))
+
+	result, err = prov.DownTo(ctx, 0)
+	require.Nil(t, err, "could not roll back migrations")
+	for _, r := range result {
+		if r.Error != nil {
+			require.Nilf(t, r.Error, "could not roll back migration %s", r.Source.Path)
+		}
+	}
+}
